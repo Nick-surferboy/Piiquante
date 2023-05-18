d53696b3d507b5bf7b7398a6582242e8183736f7
Nick <nicolas.dubigny@gmail.com>
2023-05-18 08:36:36 -0400

    Add getOne and delete controller

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -4,7 +4,11 @@ const auth = require("../middleware/auth")
 const multer = require("../middleware/multer-config")
 const router = express.Router();
 
-router.get("", auth, sauceCtrl.getAllSauce);
-router.post("", auth, multer, sauceCtrl.createSauce);
+router.get("/", auth, sauceCtrl.getAllSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.post("/:id/like", auth, multer, sauceCtrl.like);
+router.put("/:id", auth, multer,sauceCtrl.updateOneSauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
