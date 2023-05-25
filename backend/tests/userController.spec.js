be0b74c6166d092d4b61815c963a610cb1437e3c
Nick <nicolas.dubigny@gmail.com>
2023-05-25 16:24:55 -0400

    Add createUser test

--- backend/tests/userController.spec.js
+++ backend/tests/userController.spec.js
@@ -1,4 +1,7 @@
 import { createUser } from "../controllers/user";
+import bcrypt from "bcrypt";
+import uniqueValidator from "mongoose-unique-validator";
+import { User } from "../models/models";
 
 const mockRequest = () => {
   return {
@@ -11,13 +14,31 @@ const mockRequest = () => {
 
 const mockResponse = () => {
   return {
-    status: jest.fn().mockReturnthis(),
-    json: jest.fn().mockReturnthis(),
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
   };
+};
+
+const mockUser = {
+  _id: "646d527f29d40fcd020f3948",
+  email: "test@test",
+  password: "hashedPassword",
 };
 
 describe("Register User", () => {
   it("Should create a new user", async () => {
-    // await createUser()
+    jest.spyOn(bcrypt, "hash").mockResolvedValueOnce("hashedPassword");
+    jest.spyOn(User, "create").mockResolvedValueOnce(mockUser);
+
+    const mockReq = mockRequest();
+    const mockRes = mockResponse();
+    await createUser(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(201);
+    expect(bcrypt.hash).toHaveBeenCalledWith("secret", 10);
+    expect(User.create).toHaveBeenCalledWith({
+      email: "test@test",
+      password: "hashedPassword",
+    });
   });
 });
